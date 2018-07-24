05f6ad1129a34b0c7e9b88ba705c1b63acc54ca5
U-STADDOM1\vostom <vostom@STAPR152M.stadlerrail.ch>
2018-07-24 02:55:52 +0200

    The script finished.

--- CANOpenAnalyzator/CANOpenAnalyzator/CANOpenAnalyzator.py
+++ CANOpenAnalyzator/CANOpenAnalyzator/CANOpenAnalyzator.py
@@ -5,36 +5,68 @@ import string
 class NMTStartStopService:
     'Common class for the CANOpen NMT Start/Stop service'
     NMTStartStopServiceCount = 0
-    def __init__ (self, CANID, cs, NodeID):
+    def __init__ (self, CANID, cs, NodeID, TimeOffset):
         self.CANID = CANID
         self.cs = cs
         self.NodeID = NodeID
+        self.TimeOffset = TimeOffset
         NMTStartStopService.NMTStartStopServiceCount += 1
 
     def DisplayDecoded(self):
-        if (cs == 1):
-            print ("start " + "Node-ID"  + NodeID)
-        elif (cs == 2):
-            print ("stop " + "Node-ID"  + NodeID)
-        elif (cs == 128):
-            print ("enter pre-operational " + "Node-ID"  + NodeID)
-        elif (cs == 129):
-            print ("reset node " + "Node-ID"  + NodeID)
-        elif (cs == 130):
-            print ("reset communication " + "Node-ID"  + NodeID)
+        if (self.cs == "1"):
+            print (self.TimeOffset + "!!! start " + "Node-ID %d" % int(self.NodeID,16))
+        elif (self.cs == "2"):
+            print (self.TimeOffset + "!!! stop " + "Node-ID %d" % int(self.NodeID,16))
+        elif (self.cs == "80"): #128 
+            print (self.TimeOffset + "!!! enter pre-operational " + "Node-ID %d" % int(self.NodeID,16))
+        elif (self.cs == "81"): #129
+            print (self.TimeOffset + "!!! reset node " + "Node-ID %d" % int(self.NodeID,16))
+        elif (self.cs == "82"): #130
+            print (self.TimeOffset + "!!! reset communication " + "Node-ID %d" % int(self.NodeID,16))
+
+
+class NMTErrorControl:
+    'Common class for the CANOpen NMT Error Control'
+    NMTErrorControlCount = 0
+    def __init__ (self, CANID, r, s, TimeOffset):
+        self.CANID = CANID
+        self.r = r
+        self.s = s
+        self.NodeID = int(self.CANID,16) - 1792
+        self.TimeOffset = TimeOffset
+        NMTErrorControl.NMTErrorControlCount += 1
+
+    def DisplayDecoded(self):
+        if (self.s == "00"):
+            print (self.TimeOffset + "--- Boot-up " + "Node-ID %d" % self.NodeID)
+        elif (self.s == "04"):
+            print (self.TimeOffset + "--- Stopped " + "Node-ID %d" % self.NodeID)
+        elif (self.s == "05"): 
+            print (self.TimeOffset + " Operational " + "Node-ID %d" % self.NodeID)
+        elif (self.s == "7F"): #127
+            print (self.TimeOffset + "--- Pre-operational " + "Node-ID %d" % self.NodeID)
+        
+
 
 
 if __name__ == '__main__':
     if (len(sys.argv) == 1):
         print ("Start the script with the name of the log file stated as the parameter.")
     else:
-       with open(argv[1],'r',encoding="utf8") as inpFile:
+       with open(sys.argv[1],'r',encoding="utf8") as inpFile:
            for line in inpFile:
-               words = line.split(' ')
-               if (words[3] == "0000"):
-                   NMTStartStopServiceInstance = NMTStartStopService(0, words[6], words[7])
-                   NMTStartStopServiceInstance.DisplayDecoded()
-
+               rawwords = line.split(' ')
+               words = []
+               for tempword in rawwords:
+                   if len(tempword) > 0:
+                       words.append(tempword)
+               if len(words) > 3:
+                   if (words[3] == "0000"):
+                       NMTStartStopServiceInstance = NMTStartStopService(words[3], words[6], words[7], words[1])
+                       NMTStartStopServiceInstance.DisplayDecoded()
+                   if (words[3] >= "0700" and words[3] < "0800"):
+                       NMTErrorControlInstance = NMTErrorControl(words[3], 0, words[6],  words[1])
+                       NMTErrorControlInstance.DisplayDecoded()
         
 
 
